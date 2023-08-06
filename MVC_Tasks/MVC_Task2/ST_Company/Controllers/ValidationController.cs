8f93e9c7667f1a658911510e5bc672b6b32a8656
ahmed-hamed456 <120836776+ahmed-hamed456@users.noreply.github.com>
2023-08-06 23:07:33 +0300

    day5

---
+++ MVC_Tasks/MVC_Task2/ST_Company/Controllers/ValidationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ST_Company.Models;
+using ST_Company.Models.ViewModels;
+
+namespace ST_Company.Controllers
+{
+    public class ValidationController : Controller
+    {
+        Company_Context context;
+        public ValidationController()
+        {
+            context = new Company_Context();
+        }
+
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeVM addEmployeeVM)
+        {
+            if (ModelState.IsValid) { 
+            Employee employee = new Employee() {
+                ID = addEmployeeVM.ID,
+                Name = addEmployeeVM.Name,
+                Age = addEmployeeVM.Age,
+                Email = addEmployeeVM.Email,
+                Password = addEmployeeVM.Password
+            };
+                context.Employees.Add(employee);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Employee");
+            }
+            else{
+                return View(addEmployeeVM);
+            }
+
+            
+        }
+    }
+}
---
+++ MVC_Tasks/MVC_Task2/ST_Company/Models/ViewModels/AddEmployeeVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ST_Company.Models.ViewModels
+{
+    public class AddEmployeeVM
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage =".")]
+        [StringLength(50,MinimumLength =5,ErrorMessage ="Name must be from 5 to 50")]
+        public string Name { get; set; }
+        [Range(20,35,ErrorMessage ="Age must be between 20 and 35")]
+        public int Age { get; set; }
+        [DataType(DataType.EmailAddress,ErrorMessage ="You must give avalid email")]
+        [EmailAddress()]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$",ErrorMessage = "should not start with digit or special character\r\nshould not end with special character must contain at least one letter\r\nmust contain at least one digit\r\nlength should be minimum 8 character")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name="Confirm password")]
+        [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$", ErrorMessage = "should not start with digit or special character\r\nshould not end with special character must contain at least one letter\r\nmust contain at least one digit\r\nlength should be minimum 8 character")]
+        [Compare("Password",ErrorMessage = "Password must match confirm password")]
+        public string confirmPassword { get; set; }
+    }
+}
